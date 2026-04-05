ebe235c6022650254db66428cd25f52a463deb9a
filipposserris@gmail.com <filipposserris@gmail.com>
2026-04-05 09:33:33 +0300

    Basic types for auth and tasks

---
+++ frontend/src/types/AuthResponse.ts
@@ -0,0 +1,5 @@
+export type AuthResponse = {
+    token: string;
+    email: string;
+    fullName: string;
+};
---
+++ frontend/src/types/RegisterRequest.ts
@@ -0,0 +1,5 @@
+export type RegisterRequest = {
+    fullname:string,
+    email:string,
+    password:string
+};
